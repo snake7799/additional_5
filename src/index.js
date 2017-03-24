43d440a566affd76bb669155e04222938707456d
Pavel Egorov <snake7799@users.noreply.github.com>
2017-03-24 12:16:13 +0300

    brackets/feature: implement check method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	if (str.length % 2 !== 0)
+		return false;
+	
+	let bracketsArr = str.split('');
+	let stack = [];
+	
+	for (let i = 0; i < bracketsArr.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if ((stack[stack.length - 1] == bracketsConfig[j][0]) && (bracketsArr[i] == bracketsConfig[j][1]))
+				stack.pop();
+			else if (bracketsArr[i] == bracketsConfig[j][0])
+				stack.push(bracketsArr[i]);
+		}
+
+	}
+	
+	return stack.length === 0;
+};
